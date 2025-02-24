50f63e5716678e5c7d2b096c46656cb4dc1ca004
SoumitraUtturkar <soumitravutturkar@gmail.com>
2025-02-24 20:32:03 +0530

    modify

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+import User from '../models/userModel.js';
+
+// Get all users
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+// Create a new user
+export const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid User Data' });
+    }
+};

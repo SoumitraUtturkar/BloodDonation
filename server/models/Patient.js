0084b2a0b56a3ed294c542dc5875ace862cf58ae
Anni2818 <aniruddhakulkarni1175@gmail.com>
2025-03-02 16:02:48 +0530

    added the patients model

---
+++ server/models/Patient.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+    patientID: { type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true }, // Link to User model
+    patient_name: { type: String, required: true },
+    relation: { type: String, required: true },
+    guardian_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    bloodType: { 
+        type: String, 
+        required: true,
+        enum: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
+    },
+    location: { type: String, required: true },
+    photo : String
+},{ timestamps: true });
+
+module.export = mongoose.model("Patient", patientSchema) 

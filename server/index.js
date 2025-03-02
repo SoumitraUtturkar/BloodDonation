3d9f5f18cd5e8e138362f72ce44dad1ec0c3c943
SoumitraUtturkar <soumitravutturkar@gmail.com>
2025-03-02 15:46:25 +0530

    schema

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ dbConnect();
 
 app.use(express.json())
 const PORT = process.env.PORT ||4000
-const user = require("./routes/user")
+const user = require("./routes/user");
+const Donor = require('./models/Donor');
 //routes
 app.use("/api/v1", user);
+
 
 app.get('/', (req,res)=>{
     res.send('Welcome to my APP')
